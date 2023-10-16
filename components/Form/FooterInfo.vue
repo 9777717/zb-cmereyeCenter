938680815e61c997afc20a4f3353c1bb1a91292b
jiangzf <xiaojiangzf@163.com>
2023-10-16 16:55:27 +0800

    fix: edit form rules

--- components/Form/FooterInfo.vue
+++ components/Form/FooterInfo.vue
@@ -57,14 +57,15 @@ const rules = reactive<FormRules>({
       trigger: 'change',
     },
   ],
+  desc: [{ required: true, message: '請輸入訊息', trigger: 'change' }],
 })
 
 const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
     if (valid) {
-      postData()
-      commitToCms()
+      // postData()
+      // commitToCms()
       resetForm(formEl) // 成功提交清空表单内容
     } else {
     }
