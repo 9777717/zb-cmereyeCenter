21af9e578ed9900d439270f33831bdbe9a671ee1
jiangzf <xiaojiangzf@163.com>
2024-03-21 15:56:03 +0800

    fix: 修改侧边

--- components/Page/Content/onlinContact.vue
+++ components/Page/Content/onlinContact.vue
@@ -26,7 +26,7 @@ const rt = ref(route)
     ]">
     <div>
       <a class="topitem ga-pc-right-ljyy" id="ga-pc-right-ljyy" href="https://mqj.zoosnet.net/LR/Chatpre.aspx?id=MQJ40126824&cid=7f3c58ea65c34d9d82c1f6455384212f&lng=big5&sid=cd5457bae7eb4c9db0534553310cb509&p=https%3A//hkcmereye.com/&rf1=&rf2=&msg=&e=hkcmereye.com[youce-goutong]&d=1692676040714">
-        <img class="ga-pc-right-ljyy" v-if="locale !== 'en'" id="ga-pc-right-ljyy" src="https://static.cmereye.com/imgs/2023/07/5e1ee955594fae19.png" alt="希瑪眼科中心_立即預約眼科服務" title="希瑪眼科中心_立即預約眼科服務">
+        <img class="ga-pc-right-ljyy" v-if="locale !== 'en'" id="ga-pc-right-ljyy" src="https://static.cmereye.com/imgs/2024/03/d238c8c20a635d6f.png" alt="希瑪眼科中心_立即預約眼科服務" title="希瑪眼科中心_立即預約眼科服務">
         <img class="ga-pc-right-ljyy" v-if="locale === 'en'" id="ga-pc-right-ljyy" src="https://static.cmereye.com/imgs/2023/09/a216a8f766bc0d89.png" alt="希瑪眼科中心_立即預約眼科服務" title="希瑪眼科中心_立即預約眼科服務">
       </a>
       <a class="ocitem ga-pc-right-zxcx" id="ga-pc-right-zxcx" href="https://mqj.zoosnet.net/LR/Chatpre.aspx?id=MQJ40126824&cid=149cdac9b31444cd81512a1ce54d1ae9&lng=big5&sid=6ac3c6650aef4b099f1018a3cd8a7887&p=https//www.hkcmereye.com/&rf1=&rf2=&msg=&e=www.hkcmereye.com[youce-goutong]&d=1692675682853">
@@ -35,7 +35,7 @@ const rt = ref(route)
             <div class="ga-pc-right-zxcx">{{$t('components.content.onlinContact.chatNow')}}</div>
             <div class="ga-pc-right-zxcx">{{$t('components.content.onlinContact.chatNow3')}}</div>
           </div>
-          <div class="ga-pc-right-zxcx"><img class="ga-pc-right-zxcx" src="https://static.cmereye.com/imgs/2023/07/80e19816d1bcd08e.png" alt=""></div>
+          <div class="ga-pc-right-zxcx"><img class="ga-pc-right-zxcx" src="https://static.cmereye.com/static/hkeyecmer/svg/icon_1001.svg" alt=""></div>
         </div>
       </a>
       <a class="ocitem ga-pc-right-WhatsApp" id="ga-pc-right-WhatsApp" href="https://api.whatsapp.com/send?phone=85293451508&text=%E4%BD%A0%E5%A5%BD,%E6%88%91%E6%83%B3%E6%9F%A5%E8%A9%A2" target="_blank">
@@ -44,7 +44,7 @@ const rt = ref(route)
             <div class="ga-pc-right-WhatsApp">WhatsApp</div>
             <div class="ga-pc-right-WhatsApp">9345 1508</div>
           </div>
-          <div class="ga-pc-right-WhatsApp"><img class="ga-pc-right-WhatsApp" src="https://static.cmereye.com/imgs/2023/07/b5c111de8fe784c4.png" alt=""></div>
+          <div class="ga-pc-right-WhatsApp"><img class="ga-pc-right-WhatsApp" src="https://static.cmereye.com/static/hkeyecmer/svg/icon_1002.svg" alt=""></div>
         </div>
       </a>
       <a href="tel: +852 3956 2025" class="ocitem ga-pc-right-ljzd" id="ga-pc-right-ljzd">
@@ -53,7 +53,7 @@ const rt = ref(route)
             <div class="ga-pc-right-ljzd">{{$t('components.content.onlinContact.callNow')}}</div>
             <div class="ga-pc-right-ljzd">(852) 3956 2025</div>
           </div>
-          <div class="ga-pc-right-ljzd"><img class="ga-pc-right-ljzd" src="https://static.cmereye.com/imgs/2023/07/eb96c8fed21aa580.png" alt=""></div>
+          <div class="ga-pc-right-ljzd"><img class="ga-pc-right-ljzd" src="https://static.cmereye.com/static/hkeyecmer/svg/icon_1003.svg" alt=""></div>
         </div>
       </a>
     </div>
@@ -69,6 +69,7 @@ const rt = ref(route)
   right: 83px;
   // top: 520px;
   bottom: 150px;
+  // animation: opin 1s ease-in forwards;
   &>div{
     .topitem{
       width: 160px;
@@ -78,16 +79,18 @@ const rt = ref(route)
         cursor: pointer;
         // animation: ss 3s linear infinite;
         transform-origin: bottom center;
+        transition: all .3s;
         &:hover{
           transform-origin: bottom center;
-          animation: ss 3s linear infinite;
+          transform: scale(1.05);
+          // animation: ss 3s linear infinite;
         }
       }
     }
     .ocitem{
       width: 160px;
       height: 38px;
-      border: 2px solid #8FD2E4;
+      border: 2px solid #5595DF;
       margin-top: 20px;
       border-radius: 5px;
       cursor: pointer;
@@ -102,47 +105,64 @@ const rt = ref(route)
         &>div:nth-of-type(1){
           text-align: center;
           flex: 1;
-          line-height: 34px;
+          height: 34px;
           position: relative;
-          transition: all .3s;
-          border-right: 1px solid rgba(143, 210, 228, .5);
           transition: all .3s;
           &>div{
             position: relative;
             z-index: 2;
-            font-size: 18px;
-            color: #8FD2E4;
+            font-size: 20px;
+            line-height: 32px;
+            color: #5595DF;
             transition: all .3s;
+            height: 110%;
+            font-weight: bold;
+            &:nth-of-type(2){
+              color: #fff;
+              background: #5595DF;
+            }
+            &::after{
+              content: "";
+              width: 1px;
+              height: calc(100% - 2px);
+              position: absolute;
+              bottom: 1px;
+              right: -1px;
+              background: #2958a3;
+              z-index: 1;
+              transition: all .3s;
+            }
           }
-          &::before{
-            content: "";
-            width: 100%;
-            height: 51%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            background: rgba(255, 255, 255, 1);
-            z-index: 1;
-            transition: all .3s;
-          }
-          &::after{
-            content: "";
-            width: 100%;
-            height: 51%;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            background: rgba(239, 239, 239, 1);
-            z-index: 1;
-            transition: all .3s;
-          }
+          // &::before{
+          //   content: "";
+          //   width: 100%;
+          //   height: 51%;
+          //   position: absolute;
+          //   top: 0;
+          //   left: 0;
+          //   background: rgba(255, 255, 255, 1);
+          //   z-index: 1;
+          //   transition: all .3s;
+          // }
+          // &::after{
+          //   content: "";
+          //   width: 2;
+          //   height: 100%;
+          //   position: absolute;
+          //   bottom: 0;
+          //   right: 0;
+          //   background: #2958a3;
+          //   z-index: 1;
+          //   transition: all .3s;
+          // }
         }
         &>div:nth-of-type(2){
           width: 40px;
           display: flex;
           justify-content: center;
           align-items: center;
-          border-left: 1px solid rgba(143, 210, 228, 1);
+          border-left: 1px solid #5595DF;
+          // border-left: 2px solid #fff;
           position: relative;
           transition: all .3s;
           &::before{
@@ -152,7 +172,7 @@ const rt = ref(route)
             position: absolute;
             top: 0;
             left: 0;
-            background: rgba(143, 210, 228, .5);
+            background: #80C8F1;
             z-index: 1;
           }
           &::after{
@@ -162,12 +182,12 @@ const rt = ref(route)
             position: absolute;
             bottom: 0;
             left: 0;
-            background: #8FD2E4;
+            background: #5595DF;
             z-index: 1;
           }
           img{
             position: relative;
-            width: 60%;
+            // width: 60%;
             z-index: 2;
           }
         }
@@ -191,21 +211,23 @@ const rt = ref(route)
         }
       }
       &:hover{
-        border: 2px solid #fff;
+        // border: 2px solid #fff;
+        box-shadow: 0 3px 10px rgba(128, 200, 241, .7);
+        // transform: scale(1.05);
         &>div{
-          border: 1px solid #8FD2E4;
+          // border: 1px solid #8FD2E4;
           &>div:nth-of-type(1){
             &>div{
-              color: #fff;
-              margin-top: -1px;
-              padding-top: 0px;
+              // color: #fff;
+              // margin-top: -1px;
+              // padding-top: 0px;
               transform: translateY(-100%);
             }
             &::before{
-              background: rgba(143, 210, 228, .5);
+              // background: rgba(143, 210, 228, .5);
             }
             &::after{
-              background: #8FD2E4;
+              // background: #8FD2E4;
             }
           }
         }
@@ -213,6 +235,14 @@ const rt = ref(route)
     }
   }
 }
+// @keyframes opin{
+//   0%{
+//     opacity: 0;
+//   }
+//   100%{
+//     opacity: 1;
+//   }
+// }
 @keyframes ss {
   0%{
     transform: rotate(0deg);
